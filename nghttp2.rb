3c7e395ed4ab18471e827436708c85cb23335b78
Joel Larsson <joellarsson@sent.at>
2019-08-29 16:36:16 +0200

    Initial commit

---
+++ nghttp2.rb
@@ -0,0 +1,55 @@
+class Nghttp2 < Formula
+  desc "HTTP/2 C Library"
+  homepage "https://nghttp2.org/"
+  url "https://github.com/nghttp2/nghttp2/releases/download/v1.39.2/nghttp2-1.39.2.tar.xz"
+  sha256 "a2d216450abd2beaf4e200c168957968e89d602ca4119338b9d7ab059fd4ce8b"
+
+  bottle do
+    sha256 "6d1d1e137cdb97927bb16ebdd26436b0ac7dfcb07bf8d095f1b122a2936113b2" => :mojave
+    sha256 "693ba460b5d7d0d8105f99954f88fa3a172feb71cb5a7e81fbf9d9709e77be63" => :high_sierra
+    sha256 "0dae885aa1c533925fe717d0ee888e200e2a697c7ac3f9bf0963255a22261c37" => :sierra
+  end
+
+  head do
+    url "https://github.com/nghttp2/nghttp2.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "cunit" => :build
+  depends_on "pkg-config" => :build
+  depends_on "sphinx-doc" => :build
+  depends_on "c-ares"
+  depends_on "jansson"
+  depends_on "jemalloc"
+  depends_on "libev"
+  depends_on "libevent"
+  depends_on "openssl@1.1"
+
+  def install
+    ENV.cxx11
+
+    args = %W[
+      --prefix=#{prefix}
+      --disable-silent-rules
+      --enable-app
+      --disable-python-bindings
+      --with-xml-prefix=/usr
+    ]
+
+    # requires thread-local storage features only available in 10.11+
+    args << "--disable-threads" if MacOS.version < :el_capitan
+
+    system "autoreconf", "-ivf" if build.head?
+    system "./configure", *args
+    system "make"
+    system "make", "check"
+    system "make", "install"
+  end
+
+  test do
+    system bin/"nghttp", "-nv", "https://nghttp2.org"
+  end
+end
